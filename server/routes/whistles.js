33cff43f9c95305e7b0d81488a2e44cc19f3d2a8
Cody Lobdell <thecreator85@gmail.com>
2023-06-05 11:10:19 -0700

    added models, routes code, folders with script

--- server/routes/whistles.js
+++ server/routes/whistles.js
@@ -1,1 +1,26 @@
 ``
+import express from 'express';
+import { verifyToken } from '../verifyToken.js';
+import { createWhistle, deleteWhistle, likeOrDislike, getAllWhistles, getUserWhistles, getExploreWhistles } from '../controllers/whistle.js';
+
+const router = express.Router();
+
+// Create a new whistle
+router.post('/', verifyToken, createWhistle);
+
+// Delete a whistle
+router.delete('/:id', verifyToken, deleteWhistle);
+
+// Like or Dislike Whistle
+router.put('/:id/like', likeOrDislike)
+
+// Get all timeline whistles
+router.get("/timeline/:id", getAllWhistles);
+
+// Get user whistles only
+router.get('/user/all/:id', getUserWhistles);
+
+//explore
+router.get('/explore', getExploreWhistles);
+
+export default router;
