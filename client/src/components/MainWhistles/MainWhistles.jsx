b866a86c8954c524f8c1d2823feec0ddd3a8ccad
Ryan Bagan <darcknight1980@gmail.com>
2023-06-07 13:40:46 -0700

    warnings ignored

--- client/src/components/MainWhistles/MainWhistles.jsx
+++ client/src/components/MainWhistles/MainWhistles.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState } from 'react'
 import TimelineWhistle from '../TimelineWhistle/TimelineWhistle';
 
@@ -9,10 +10,10 @@ const MainWhistles = () => {
 
   const { currentUser } = useSelector((state) => state.user);
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const submitWhistle = await axios.post("/whistles", {
+      const submitWhistle = await axios.post('/whistles', {
         userId: currentUser._id,
         description: whistleText,
       });
@@ -24,10 +25,7 @@ const MainWhistles = () => {
 
   return (
     <div>
-      {currentUser && (
-        <p className="font-bold pl-2 my-2">{currentUser.username}</p>
-      )}
-
+      {currentUser && <p className="font-bold pl-2 my-2">{currentUser.username}</p>}
 
       <form className="border-b-2 pb-6">
         <textarea
@@ -35,17 +33,65 @@ const MainWhistles = () => {
           type="text"
           maxLength={300}
           placeholder="Blow that Whistle baby!"
-          className="bg-slate-300 rounded-lg w-full p-2 text-black">
-        </textarea>
+          className="bg-slate-300 rounded-lg w-full p-2 text-black"
+        ></textarea>
         <button
           onClick={handleSubmit}
-          className="bg-red-500 text-white py-2 px-4 rounded-full ml-auto">
+          className="bg-red-500 text-white py-2 px-4 rounded-full ml-auto"
+        >
           Whistle
         </button>
       </form>
-      <TimelineWhistle />
+      <TimelineWhistle reverse={true} />
     </div>
-  )
+  );
 };
 
 export default MainWhistles;
+
+
+// const MainWhistles = () => {
+//   const [whistleText, setWhistleText] = useState('');
+
+//   const { currentUser } = useSelector((state) => state.user);
+
+//   const handleSubmit = async(e) => {
+//     e.preventDefault();
+//     try {
+//       const submitWhistle = await axios.post("/whistles", {
+//         userId: currentUser._id,
+//         description: whistleText,
+//       });
+//       window.location.reload(false);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       {currentUser && (
+//         <p className="font-bold pl-2 my-2">{currentUser.username}</p>
+//       )}
+
+
+//       <form className="border-b-2 pb-6">
+//         <textarea
+//           onChange={(e) => setWhistleText(e.target.value)}
+//           type="text"
+//           maxLength={300}
+//           placeholder="Blow that Whistle baby!"
+//           className="bg-slate-300 rounded-lg w-full p-2 text-black">
+//         </textarea>
+//         <button
+//           onClick={handleSubmit}
+//           className="bg-red-500 text-white py-2 px-4 rounded-full ml-auto">
+//           Whistle
+//         </button>
+//       </form>
+//       <TimelineWhistle />
+//     </div>
+//   )
+// };
+
+// export default MainWhistles;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
 
       <div className="px-0 md:px-6 mx-auto">
         <SearchIcon className="absolute m-2" />
-        <input type="text" className="bg-yellow-400 rounded-full py-2 px-8" />
+        <input type="text" placeholder="Search it baby!" className="bg-yellow-400 rounded-full py-2 px-8" />
       </div>
     </div>
 
