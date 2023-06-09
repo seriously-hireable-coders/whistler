0b99e11b1e6f5872100a5051f948ed6ebbe740ae
Thomas Lau <tomslau@outlook.com>
2023-06-08 18:36:53 -0700

    test complete

--- client/src/__tests__/MainWhistles.test.js
+++
@@ -1,9 +0,0 @@
-import MainWhistles from '../components/MainWhistles/MainWhistles';
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { renderWithProviders } from './utils-for-tests';
-
-xtest('Signin/SignUp: user able to type', () => {
-  renderWithProviders(<MainWhistles/>);
-  screen.getByRole('')
-});
--- client/src/__tests__/utils-for-tests.js
+++ client/src/__tests__/utils-for-tests.js
@@ -4,45 +4,17 @@ import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 // As a basic setup, import your same slice reducers
 import userSlice from '../redux/userSlice';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist'
-import storage from 'redux-persist/lib/storage';
-// export function renderWithProviders(
-//   ui,
-//   {
-//     preloadedState = {
-//       currentUser: {"_id":{"$oid":"647b3b87c91be26126908b09"},"username":"thomasl","email":"thomasl@gmail.com","password":"$2a$10$9/VT8GA5Mxlch9cKZ7dnpulKXOLQhTa4HqD./Heu874.6KhE8KzIy","followers":["647b1d57a425d84b22608ebe"],"following":[],"createdAt":{"$date":{"$numberLong":"1685797767747"}},"updatedAt":{"$date":{"$numberLong":"1686160631387"}},"__v":{"$numberInt":"0"},"profilePicture":"https://firebasestorage.googleapis.com/v0/b/whistler-b627a.appspot.com/o/1686089593024Mi%20(2).jpg?alt=media&token=d9e051ff-2170-4162-86f5-123566db9716"},
-//       isLoading: false,
-//       error: false,
-//     },
-//     // Automatically create a store instance if no store was passed in
-//     store = configureStore({ reducer: { user: userSlice.reducer }, preloadedState }),
-//     ...renderOptions
-//   } = {}
-// ) {
-//   function Wrapper({ children }) {
-//     return <Provider store={store}>{children}</Provider>
-//   }
-
-//   // Return an object with the store and all of RTL's query functions
-//   return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
-// }
-
 
 export function renderWithProviders(
   ui,
   {
-    preloadedState = {},
+    preloadedState = {
+      currentUser: {"_id":{"$oid":"354"},"username":"aa","email":"aa@gmail.com","password":"343y","followers":["er"],"following":[],"createdAt":{"$date":{"$numberLong":"454"}},"updatedAt":{"$date":{"$numberLong":"54"}},"__v":{"$numberInt":"0"},"profilePicture":"sth.jpg"},
+      isLoading: false,
+      error: false,
+    },
     // Automatically create a store instance if no store was passed in
-    store = configureStore({ reducer: { user: userSlice }, preloadedState }),
+    store = configureStore({ reducer: { user: userSlice.reducer }, preloadedState }),
     ...renderOptions
   } = {}
 ) {
@@ -50,9 +22,8 @@ export function renderWithProviders(
     return <Provider store={store}>{children}</Provider>
   }
 
-  // Return an object with the store and all of RTL's query functions
+
   return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
 }
-
 
 xtest('not a test', () => {});
